2d8799e801dd4562acd15a55621dd46eb1377868
Peter Nielsen <peterflemmingnielsen@gmail.com>
2017-09-04 13:27:27 +0200

    first commit

---
+++ src/main/java/com/github/dronezcc/riser/gui/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.github.dronezcc.riser.gui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
