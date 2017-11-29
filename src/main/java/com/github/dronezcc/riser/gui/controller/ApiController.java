a57b35bae5baee217854a42d4aba141e056a759a
Peter Nielsen <peterflemmingnielsen@gmail.com>
2017-11-29 17:13:48 +0100

    THis update

--- src/main/java/com/github/dronezcc/riser/gui/controller/ApiController.java
+++ src/main/java/com/github/dronezcc/riser/gui/controller/ApiController.java
@@ -2,7 +2,10 @@ package com.github.dronezcc.riser.gui.controller;
 
 import com.github.dronezcc.riser.gui.domain.User;
 import com.github.dronezcc.riser.gui.domain.UserRepository;
+import com.github.dronezcc.riser.gui.domain.UserRole;
+import com.github.dronezcc.riser.gui.services.UserRoleService;
 import com.github.dronezcc.riser.gui.services.UserService;
+import org.apache.catalina.UserDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,9 @@ public class ApiController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    UserRoleService userRoleService;
+
 
     @RequestMapping("/api/users")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -34,6 +40,38 @@ public class ApiController {
         return userService.findAll();
     }
 
+    @RequestMapping("/api/secret_password")
+    public void secretPassword(@RequestParam("password") String password, @RequestParam("name") String name) throws Exception {
+        BCryptPasswordEncoder bCryptPasswordEncoder =  new BCryptPasswordEncoder();
+        String encoded_pass = bCryptPasswordEncoder.encode(password);
+
+        User user = userService.findByUserName(name);
+        user.setPassword(encoded_pass);
+        User us = userService.save(user);
+    }
+
+
+    @RequestMapping(value = "/api/users/create", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public User newUser(@RequestParam("username") String username, @RequestParam("email") String email) throws Exception {
+        User user = new User();
+        user.setEmail(email);
+        user.setUserName(username);
+        user.setEnabled(1);
+        User savedUser = userService.save(user);
+
+        UserRole ur = new UserRole();
+        ur.setUserid(savedUser.getUserid());
+        ur.setRole("ROLE_ADMIN");
+
+        userRoleService.save(ur);
+        UserRole ur_user = new UserRole();
+        ur_user.setUserid(savedUser.getUserid());
+        ur_user.setRole("ROLE_USER");
+        userRoleService.save(ur_user);
+
+        return savedUser;
+    }
 
     @RequestMapping(value = "/api/users/set_password", method = RequestMethod.POST)
     @PreAuthorize("hasRole('ROLE_USER')")
--- src/main/java/com/github/dronezcc/riser/gui/services/UserRoleService.java
+++ src/main/java/com/github/dronezcc/riser/gui/services/UserRoleService.java
@@ -1,6 +1,7 @@
 package com.github.dronezcc.riser.gui.services;
 
 
+import com.github.dronezcc.riser.gui.domain.UserRole;
 import com.github.dronezcc.riser.gui.domain.UserRolesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class UserRoleService {
 
     @Autowired
     UserRolesRepository userRolesRepository;
+
+    public UserRole save(UserRole userRole)
+    {
+        return userRolesRepository.save(userRole);
+    }
 
     public List<String> findRoleByUserName(String name) {
         return userRolesRepository.findRoleByUserName(name);
